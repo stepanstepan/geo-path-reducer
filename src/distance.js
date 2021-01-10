58c93a7ebb5c83cfd85ef930685d98f0e29955c0
Stepan Smirnov <stepan@microfunk.ru>
2021-01-10 11:58:21 +0100

    updating point format

---
+++ src/distance.js
@@ -0,0 +1,15 @@
+export default function distance(lat1, lon1, lat2, lon2) {
+  // Calculate the great-circle distance between two points on the Earth's surface
+  const R = 6371; // Earth's radius in kilometers
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+  return d;
+}
--- src/perpendicular-distance.js
+++ src/perpendicular-distance.js
@@ -1,9 +1,11 @@
+import distance from "./distance";
+
 export default function perpendicularDistance(point, start, end) {
   // Calculate the perpendicular distance from point to line defined by start and end
-  const x = point[0], y = point[1];
-  const x1 = start[0], y1 = start[1];
-  const x2 = end[0], y2 = end[1];
-  const numerator = Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1);
-  const denominator = Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
-  return numerator / denominator;
+  const d1 = distance(point.latitude, point.longitude, start.latitude, start.longitude);
+  const d2 = distance(point.latitude, point.longitude, end.latitude, end.longitude);
+  const d3 = distance(start.latitude, start.longitude, end.latitude, end.longitude);
+  const s = (d1 + d2 + d3) / 2;
+  const area = Math.sqrt(s * (s - d1) * (s - d2) * (s - d3));
+  return (area * 2) / d3;
 }
